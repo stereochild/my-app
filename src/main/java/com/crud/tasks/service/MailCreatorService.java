080c33efb9d52306c7ffb52045235e16009aa8cc
stereochild <kamil.drozdz@gmail.com>
2017-12-09 15:54:52 +0100

    adding details to mail template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
+
+    @Autowired
+    private CompanyConfig companyConfig;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -20,9 +24,11 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("tasks_url", "https://stereochild.github.io/");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", "See you later aligator!");
+        context.setVariable("company_details", companyConfig.getCompanyName() + "\n" + companyConfig.getCompanyPhone() + " " + companyConfig.getCompanyMail() + "\n" + companyConfig.getCompanyGoal());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
