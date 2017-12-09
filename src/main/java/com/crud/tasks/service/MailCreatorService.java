98d1786b2a0a26a6b55de69e8483d142fbc8abc8
stereochild <kamil.drozdz@gmail.com>
2017-12-09 16:06:17 +0100

    ...

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -24,7 +24,7 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "https://stereochild.github.io/");
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbye_message", "See you later aligator!");
